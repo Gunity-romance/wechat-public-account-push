fa81e3a957efc0c10330cc6c940a0ff2d99f6d6f
Gunity-romance <114277240+Gunity-romance@users.noreply.github.com>
2022-09-24 18:48:03 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oW-tB6SL0leYaObvMMmwAPQEqV98",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "♥",
+      useTemplateId: "5jYywHXlIAt_5IjqwzjUhutzXRGql9LdNZSg2Az0AhM",
       // 所在省份
       province: "河南",
       // 所在城市
@@ -97,7 +97,7 @@ export const USER_CONFIG = {
     {
       name: "小荣",
       id: "oW-tB6QFTQX0K-HAorLuGjBC14O8",
-      useTemplateId: "早上好呀",
+      useTemplateId: "5jYywHXlIAt_5IjqwzjUhutzXRGql9LdNZSg2Az0AhM",
       province: "河南",
       city: "许昌",
       horoscopeDate: '07-02',
